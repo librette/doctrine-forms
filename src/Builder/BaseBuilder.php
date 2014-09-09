da4cc862746500b92dcd5d0b79d078d209e93bd9
David Matejka <matej21@matej21.cz>
2014-09-10 00:26:37 +0200

    phpdoc typo

--- src/Builder/BaseBuilder.php
+++ src/Builder/BaseBuilder.php
@@ -10,7 +10,7 @@ use Nette\Object;
 abstract class BaseBuilder extends Object implements IBuilder
 {
 
-	/** @var ContainerBuilder */
+	/** @var IBuilder */
 	protected $parent;
 
 	/** @var IComponent */
