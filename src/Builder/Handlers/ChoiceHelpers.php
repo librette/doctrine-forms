0af432dd8d257d6c10ae126c66cac6e529a51b64
David Matejka <matej21@matej21.cz>
2015-01-11 17:04:56 +0100

    Builder - ManyToOne: support for advanced expressions for pairs value

--- src/Builder/Handlers/ChoiceHelpers.php
+++ src/Builder/Handlers/ChoiceHelpers.php
@@ -1,6 +1,7 @@
 <?php
 namespace Librette\Doctrine\Forms\Builder\Handlers;
 
+use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Kdyby\Doctrine\EntityDao;
 
@@ -15,15 +16,43 @@ class ChoiceHelpers
 
 	public static function getPairs(EntityDao $dao, array $options = [])
 	{
-		$options += ['criteria' => [], 'orderBy' => [], 'key' => NULL];
+		$options += ['criteria' => [],
+		             'orderBy'  => [],
+		             'key'      => $dao->getClassMetadata()->getSingleIdentifierFieldName()
+		];
 		if (empty($options['value'])) {
 			$options['value'] = self::getValueFieldFallback($dao->getClassMetadata());
 		}
 		if (is_string($options['orderBy'])) {
 			$options['orderBy'] = [$options['orderBy'] => 'asc'];
 		}
+		if ($dao->getClassMetadata()->hasField($options['value'])) {
+			return $dao->findPairs($options['criteria'], $options['value'], $options['orderBy'], $options['key']);
+		} else {
+			return self::getPairsAdvanced($dao, $options);
+		}
+	}
 
-		return $dao->findPairs($options['criteria'], $options['value'], $options['orderBy'], $options['key']);
+
+	private static function getPairsAdvanced(EntityDao $dao, array $options)
+	{
+		$query = $dao->createQueryBuilder('e')
+		             ->whereCriteria($options['criteria'])
+		             ->select("e")
+		             ->resetDQLPart('from')->from($dao->getClassName(), 'e', 'e.' . $options['key'])
+		             ->autoJoinOrderBy((array) $options['orderBy'])
+		             ->getQuery();
+		$parts = explode('.', $options['value']);
+
+		$result = $query->getResult();
+
+		return array_map(function ($value) use ($parts) {
+			foreach ($parts as $part) {
+				$value = $value->$part;
+			}
+
+			return $value;
+		}, $result);
 	}
 
 
