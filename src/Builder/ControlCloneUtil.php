237658db0485465d27086bb909b77331f4e28d81
David Matejka <matej21@matej21.cz>
2020-10-20 14:40:16 +0200

    clonning: fix clonning monitors in a component

--- src/Builder/ControlCloneUtil.php
+++ src/Builder/ControlCloneUtil.php
@@ -1,6 +1,7 @@
 <?php
 namespace Librette\Doctrine\Forms\Builder;
 
+use Nette\ComponentModel\Component;
 use Nette\ComponentModel\IComponent;
 use Nette\Forms\Controls\BaseControl;
 use Nette\Forms\Rules;
@@ -19,8 +20,29 @@ final class ControlCloneUtil
 		if ($newControl instanceof BaseControl) {
 			self::deepClone($control, $newControl);
 		}
+		self::cloneComponentMonitors($newControl, $control);
 
 		return $newControl;
+	}
+
+
+	public static function cloneComponentMonitors(Component $component, Component $prototype)
+	{
+		$clonner = function () use ($prototype) {
+			/** @var Component $this */
+			foreach ($this->monitors as &$monitors) {
+				$monitorcallbacks = &$monitors[3];
+				foreach ($monitorcallbacks as &$callbacks) {
+					foreach ($callbacks as &$cb) {
+						if (is_array($cb) && $cb[0] === $prototype && ($cb[1] === 'attached' || $cb[1] === 'dettached')) {
+							$cb[0] = $this;
+						}
+					}
+				}
+			}
+		};
+		$clonner = $clonner->bindTo($component, Component::class);
+		$clonner();
 	}
 
 
